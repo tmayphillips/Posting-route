8c25e05be5d479cdf6242f35f1dbe8362e6b2519
TIFFANY MAY-PHILLIPS <tiffanymay-phillips@TIFFANYs-MBP.localdomain>
2019-03-25 14:59:56 -0500

    dynamic routing

---
+++ client/main.js
@@ -0,0 +1,23 @@
+
+
+let sendBtn = document.getElementById('sendBtn')
+let firstNameText = document.getElementById('firstNameText')
+let lastNameText = document.getElementById('lastNameText')
+
+sendBtn.addEventListener('click',function() {
+  let firstname = firstNameText.value
+  let lastname =  lastNameText.value
+
+  fetch('http://localhost:3000/fullname', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      userFirstName: firstname,
+      userLastName: lastname,
+    })
+  }).then(response => {
+      return response.json()
+  }).then(json => console.log(json))
+  })
---
+++ server/app.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+// const hostname = '127.0.0.1';
+const port = 3000;
+
+const express = require('express')
+const server = express()
+const bodyParser = require('body-parser')
+
+var cors = require('cors')
+server.use(cors())
+server.use(bodyParser.json())
+
+server.get('/', function(req,res) {
+  res.send(`The book`);
+})
+
+server.post('/fullname',(req,res) => {
+  console.log(req.body);
+  let firstname = req.body.userFirstName
+  let lastname = req.body.userLastName
+
+  console.log(firstname);
+  console.log(lastname);
+
+  res.json({firstname: firstname, lastname: lastname})
+})
+
+server.listen(port, () => {
+  console.log(`Server running at http://${port}/`);
+});
